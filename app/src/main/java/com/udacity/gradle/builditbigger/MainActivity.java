e3349b659fbffc0acbbe9330ee11f0cd75d54584
Clemente Morales Fernandez <morales.clements@gmail.com>
2016-07-06 21:43:35 -0400

    BuildItBigger feature Integrate Application with Google cloud endpoint service to get the jokes

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,13 +1,14 @@
 package com.udacity.gradle.builditbigger;
 
-import android.content.Intent;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.util.Pair;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-import com.udacity.gradle.builditbigger.jokedisplayer.views.JokeDisplayerActivity;
+import com.udacity.gradle.builditbigger.services.JokeRequestorAsyncTask;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -37,11 +38,6 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void tellJoke(View view){
-        // String joke = jokeProvider.getRandomJoke();
-        String joke = "Jajaja";
-        // Toast.makeText(this, joke, Toast.LENGTH_SHORT).show();
-        Intent intent = new Intent(this, JokeDisplayerActivity.class);
-        intent.putExtra(JokeDisplayerActivity.JOKE_EXTRA_KEY, joke);
-        startActivity(intent);
+        new JokeRequestorAsyncTask().execute(new Pair<Context, String>(this, "Lawyers"));
     }
 }
